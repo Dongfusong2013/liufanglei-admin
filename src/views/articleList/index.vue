d0c0884729c94535f879f0e28f347971b770e586
dongfusong <dongfusong@126.com>
2019-08-21 15:53:39 +0800

    commit

--- src/views/articleList/index.vue
+++ src/views/articleList/index.vue
@@ -1,30 +1,31 @@
 <template>
   <div class="app-container">
-    <el-table v-loading="listLoading" :data="list" element-loading-text="Loading" border fit highlight-current-row>
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      element-loading-text="Loading"
+      border
+      fit
+      highlight-current-row
+    >
       <el-table-column align="center" label="ID" width="95">
-        <template slot-scope="scope">
-          {{ scope.$index }}
-        </template>
+        <template slot-scope="scope">{{ scope.$index }}</template>
       </el-table-column>
       <el-table-column label="标题" width="110">
-        <template slot-scope="scope">
-          {{ scope.row.articleTitle }}
-        </template>
+        <template slot-scope="scope">{{ scope.row.articleTitle }}</template>
       </el-table-column>
       <el-table-column label="副标题" width="110" align="center">
         <template slot-scope="scope">
           <span>{{ scope.row.articleSubTitle }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="摘要"  align="center">
-        <template slot-scope="scope">
-          {{ scope.row.articleSummary }}
-        </template>
+      <el-table-column label="摘要" align="center">
+        <template slot-scope="scope">{{ scope.row.articleSummary }}</template>
       </el-table-column>
       <el-table-column label="封面图片" width="210" align="center">
         <template slot-scope="scope">
           <div class="pic-size item-center">
-          <img :src="scope.row.picSrc" alt="" height="100%" width="100%">
+            <img :src="scope.row.picSrc" alt height="100%" width="100%" />
           </div>
         </template>
       </el-table-column>
@@ -42,72 +43,92 @@
       <el-table-column align="center" label="Actions" width="120">
         <template slot-scope="scope">
           <router-link :to="'/articleManage/edit/'+scope.row.id">
-            <el-button type="primary" size="small" icon="el-icon-edit">
-              编辑
-            </el-button>
+            <el-button type="primary" size="small" icon="el-icon-edit">编辑</el-button>
           </router-link>
         </template>
       </el-table-column>
     </el-table>
+
+    <div>      
+      <Pagination
+        v-show="total>0"
+        :total="total"
+        :page.sync="pageNo"
+        :limit.sync="pageCount"
+        @pagination="fetchData"
+      />
+    </div>
+    
   </div>
 </template>
 
 <script>
-  import {
-    getArticleList
-  } from '@/api/article'
+import { getArticleList } from "@/api/article";
+import Pagination from "@/components/Pagination";
 
-  export default {
-    filters: {
-      statusFilter(status) {
-        const statusMap = {
-          published: 'success',
-          draft: 'gray',
-          deleted: 'danger'
-        }
-        return statusMap[status]
-      }
-    },
-    data() {
-      return {
-        list: null,
-        listLoading: true
-      }
-    },
-    beforeMount() {
-      this.fetchData()
-      console.log("before mount....");
-    },
-    beforeUpdate() {
-      // this.fetchData()
-      console.log("before Update....");
-    },
-    methods: {
-      fetchData() {
-        this.listLoading = true
-        getArticleList({
-          pageNo:0,
-          pageSize:10,
-          articleType:'ideaArticle'
-        }).then(response => {
-          this.list = response.data.content;
-          console.log("=====get article list", this.list);
-          this.listLoading = false
-        })
-      }
+export default {
+  components: {
+    Pagination
+  },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: "success",
+        draft: "gray",
+        deleted: "danger"
+      };
+      return statusMap[status];
+    }
+  },
+  data() {
+    return {
+      list: null,
+      pageNo: 1,
+      pageSize: 10,
+      total: 0,
+      pageCount:0,
+      listLoading: true
+    };
+  },
+  beforeMount() {
+    this.fetchData();
+    console.log("before mount....");
+  },
+  beforeUpdate() {
+    // this.fetchData()
+    console.log("before Update....");
+  },
+  methods: {
+    fetchData() {
+      this.listLoading = true;
+      getArticleList({
+        articleType: "ideaArticle",
+        pageNo:this.pageNo -1,
+        pageSize:this.pageSize,
+      }).then(response => {
+        console.log("=====get article list", response);
+        this.total = response.data.totalElements;
+        this.list = response.data.content;      
+        this.pageCount = response.data.totalPages;          
+        this.listLoading = false;
+      });
     }
   }
+};
 </script>
 
 <style lang="css" scoped>
-  .pic-size{
-    /* width: 50px;
+.page-size {
+  height: 40px;
+}
+.pic-size {
+  /* width: 50px;
     height: 50px; */
-  }
-  .item-center{
-      display:flex;
-      flex-direction: row;
-      align-items: center;
-      justify-content: center;
-  }
+}
+.item-center {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
 </style>
